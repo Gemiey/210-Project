c819fc099d2a54ac694a21ef7c14033e19a49381
Melhusseiny <38461529+Melhusseiny@users.noreply.github.com>
2018-12-11 10:56:24 +0200

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include "buffer.h"
+#include "Mapper.h"
+#include "quicksort.h"
+#include "terasort.h"
+#include "terasortitem.h"
+#include "thread.h"
+#include "ThreadManager.h"
+int main()
+{
+    
+    terasort(); 
+}
+void TeraSortItem
