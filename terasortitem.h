96d4c86033b6c5cbaa0c06326a8f6768a14241f0
Melhusseiny <38461529+Melhusseiny@users.noreply.github.com>
2018-12-11 10:55:24 +0200

    Update terasortitem.h

--- terasortitem.h
+++ terasortitem.h
@@ -7,17 +7,17 @@ public:
     Sortable(); 
     Sortable (Sortable & sortable); 
         virtual uint128_t key() = 0; 
-        virtual void swap (Sortable * p_sortable)=0; 
-        virtual bool operator > (Sortable * p_sortable)=0; 
-        virtual bool operator < (Sortable * p_sortable)=0; 
-        virtual bool operator >= (Sortable * p_sortable)=0; 
-        virtual bool operator <= (Sortable * p_sortable)=0; 
-        virtual bool operator == (Sortable * p_sortable)=0; 
-        virtual bool operator != (Sortable * p_sortable)=0; 
-        virtual uint16_t hash(uint16_t p_reducers)=0; 
+        virtual void swap (Sortable * p_sortable)=0; //swap functions that we will use in the quick sort 
+        virtual bool operator > (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual bool operator < (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual bool operator >= (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual bool operator <= (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual bool operator == (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual bool operator != (Sortable * p_sortable)=0; //overloading the operatprs 
+        virtual uint16_t hash(uint16_t p_reducers)=0; //setting the size of the  reducer 
         virtual ~Sortable(); 
     }; 
-    typedef struct 
+    typedef struct //struct where the file will be read in each record 
     { 
         uint8_t key[10]; 
         uint8_t b1[2]; 
@@ -26,29 +26,30 @@ public:
         uint8_t filler[48]; 
         uint8_t b3[4]; 
     }teraitem_r; 
-    class TeraSortItem : public Sortable
+    class TeraSortItem : public Sortable //c;ass of terasortitem where the items will be be sorted 
         { 
             private: 
-            teraitem_r teraitem; 
+            teraitem_r teraitem; //a teraitem of the struct 
             public:
-            TeraSortItem(); 
-            TeraSortItem (TeraSortItem & teraSortItem); 
-            void operator = (TeraSortItem * teraSortItem);
-            uint128_t key (); 
-            void swap (Sortable * p_sortable); 
-            void * operator new (size_t sz,void * v);
-            bool operator > (Sortable * p_sortable); 
-            bool operator < (Sortable * p_sortable); 
-            bool operator >= (Sortable * p_sortable); 
-            bool operator <= (Sortable * p_sortable); 
-            bool operator == (Sortable * p_sortable); 
-            bool operator != (Sortable * p_sortable); 
-            ~TeraSortItem(); 
+            TeraSortItem(); //constructor 
+            TeraSortItem (TeraSortItem & teraSortItem); //
+            void operator = (TeraSortItem * teraSortItem); //operator equal to the terasortitem 
+            uint128_t key (); //getting the random index of the key 
+            void swap (Sortable * p_sortable); //swap function that swaps the two pointers 
+            void * operator new (size_t sz,void * v); //setting a new operator 
+            TeraSortItem & operator =(TeraSortItem && TS){} //move contructor 
+            bool operator > (Sortable * p_sortable); //operator overload that takes a pointer of type class sortable where it inherets from it 
+            bool operator < (Sortable * p_sortable); //operator overload that takes a pointer
+            bool operator >= (Sortable * p_sortable); //operator overload that takes a pointer
+            bool operator <= (Sortable * p_sortable); //operator overload that takes a pointer
+            bool operator == (Sortable * p_sortable); //operator overload that takes a pointer
+            bool operator != (Sortable * p_sortable); //operator overload that takes a pointer
+            ~TeraSortItem(); //distructor 
         };
 
 
 };
-uint128_t TeraSortItem::key ()
+uint128_t TeraSortItem::key () //getting the random index for the key for the sample 
 {
     uint8_t * v = teraitem.key;
     uint128_t k = 0;
@@ -65,25 +66,25 @@ uint128_t TeraSortItem::key ()
     c[9] = v[0];
     return k;
 }
-void TeraSortItem:: operator+ (TeraSortItem *p_terasortItem)
+void TeraSortItem:: operator+ (TeraSortItem *p_terasortItem) //overloading the operator by getting the key 
 {
     this->key()+p_terasortItem->key();
 }
 
-void TeraSortItem::operator /(int x)
+void TeraSortItem::operator /(int x) //doing a devided operaypr that takes an int and devide the key with it 
 {
     this->key()/x;
 }
 
-void TeraSortItem :: operator =(TeraSortItem * TerasortItem)
+void TeraSortItem :: operator =(TeraSortItem * TerasortItem) //setting a teraitem pointer of the class to the item 
 {
     teraitem= TeraSortItem->teraitem;
 }
 
-void TeraSortItem::swap(Sortable *teraitem_r)
+void TeraSortItem::swap(Sortable *teraitem_r) //doing the swap functions 
 {
-    TeraSortItem* temp;
-    temp->teraitem=p_terasortItem->teraitem_r;
-    p_terasortItem->teraitem=this->teraitem;
-    this->teraitem_r=temp->teraitem;
+    TeraSortItem* temp; //creating a pointer of type terasortitem
+    temp->teraitem=p_terasortItem->teraitem_r; //pointer temp poiting to teraitem which points to object of teraitem_r
+    p_terasortItem->teraitem=this->teraitem; //p_terasortItem points to teraitem equal to this pointer teraitem
+    this->teraitem_r=temp->teraitem; //setting the struct teraitem_r to the temp pointer to the teraitem
 }
