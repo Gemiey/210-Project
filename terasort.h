24285c32e0ef9fac1f07da5bfe9d554b16304551
Melhusseiny <38461529+Melhusseiny@users.noreply.github.com>
2018-12-11 10:54:51 +0200

    Update terasort.h

--- terasort.h
+++ terasort.h
@@ -1,15 +1,20 @@
+#include "buffer.h"
+#include "mapper.h"
 class TeraSort
 {
     private:
-      char * p_input_filename;
-      char * p_output_filename;
-      int p_mapper_count;
-      int p_reducer_count;
-      ShuffelBuffers * shuffleBuffer;
+      char * p_input_filename; //array of char the file name that we open 
+      char * p_output_filename; //array of char the file name that we open 
+      int p_mapper_count; //the mapper count that the user puts 
+      int p_reducer_count; //the number of reducers that user inputs 
+      ShuffelBuffers * shuffleBuffer; //pointer of type shuffleBuffers 
     public:
         TeraSort(char * p_input_filename, char * p_output_filename,int p_mapper_count, int p_reducer_count);
         void sort(){
               /* You need to setup the ShuffleBuffers here*/
+              p_mapper_count=p_mapper_count; 
+              p_reducer_count=p_reducer_count; 
+              *p_input_filename = *p_input_filename;
               ThreadManager * mappersThreadManager = new ThreadManager();
               for ( int i = 0 ; i  < p_mapper_count ; i ++ )
                   mappersThreadManager->addThread(new Mapper(i,input_filename,
